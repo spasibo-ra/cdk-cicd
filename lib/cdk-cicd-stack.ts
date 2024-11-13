636013178e0c4f4b0d65336fbbad5f61b598fca3
spasibo-ra <6201285+spasibo-ra@users.noreply.github.com>
2024-11-13 16:25:23 +0200

    add pipeline

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -5,5 +5,16 @@ import { Construct } from 'constructs';
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'MainPipeline', {
+      pipelineName: 'MainPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('spasibo-ra/cdk-cicd', 'cicd-practice'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    });
   }
 }
