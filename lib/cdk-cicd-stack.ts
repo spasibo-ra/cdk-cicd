5d64d2c3614f8657702e325a36f919a083820443
spasibo-ra <6201285+spasibo-ra@users.noreply.github.com>
2024-11-13 15:23:16 +0200

    Cicd project init

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,16 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
