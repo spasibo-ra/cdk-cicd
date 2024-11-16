8c866e4fe3f81b6e43a9a6d4a52e32bce70acb4e
spasibo-ra <6201285+spasibo-ra@users.noreply.github.com>
2024-11-16 18:07:06 +0200

    added unit tests

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,25 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { Construct } from 'constructs';
-import { PipelineStage } from './pipelineStage';
+import * as cdk from "aws-cdk-lib";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { PipelineStage } from "./pipelineStage";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'MainPipeline', {
-      pipelineName: 'MainPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('spasibo-ra/cdk-cicd', 'cicd-practice'),
-        commands: [
-          'npm ci',
-          'npx cdk synth'
-        ]
-      })
+    const pipeline = new CodePipeline(this, "MainPipeline", {
+      pipelineName: "MainPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "spasibo-ra/cdk-cicd",
+          "cicd-practice"
+        ),
+        commands: ["npm ci", "npx cdk synth"],
+      }),
     });
 
-    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
-      stageName: 'test'
-    }));
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
+
+    testStage.addPre(
+      new CodeBuildStep("unit-tests", {
+        commands: ["npm ci", "npm test"],
+      })
+    );
   }
 }
--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -1,1 +1,8 @@
-test('init test', () => {});
+import { handler } from "../services/hello";
+
+describe("Hello describe test suite", () => {
+  test("handler should return status 200", async () => {
+    const result = await handler({}, {});
+    expect(result.statusCode).toBe(200);
+  });
+});
